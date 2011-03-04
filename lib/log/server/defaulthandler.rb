7bac551208563cd45fee7559447540d6c2d6ee25
Jordan Sissel <jls@semicomplete.com>
2011-03-04 15:04:58 -0800

    - add default handler (missing from previous commit)

---
+++ lib/log/server/defaulthandler.rb
@@ -0,0 +1,9 @@
+
+
+require "log/namespace"
+
+class Log::Server::DefaultHandler 
+  def receive_event(event, server, client_addr, client_port)
+    puts "#{client_addr}:#{client_port}(#{server.network}/#{server.wire}) => #{event}"
+  end # def receive_event
+end # class Log::Server::DefaultHandler 
