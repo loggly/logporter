20c9930702b629254929fedced5a0ccfa3dd33c9
Jordan Sissel <jls@semicomplete.com>
2011-03-04 11:09:09 -0800

    Use BufferedTokenenizer directly now rather than LineText and friends

--- lib/log/server/connection.rb
+++ lib/log/server/connection.rb
@@ -5,7 +5,9 @@ require "log/event"
 require "socket"
 
 class Log::Server::Connection < EventMachine::Connection
-  include EventMachine::Protocols::LineText2
+  #include EventMachine::Protocols::LineText2
+  #include EventMachine::Protocols::LineAndTextProtocol
+  #include EventMachine::Protocols::LineProtocol
   include Log::Protocol::Syslog3164
  
   def initialize(server)
@@ -41,6 +43,13 @@ class Log::Server::Connection < EventMachine::Connection
     else
       #raise "Unsupported protocol #{@server.protocol}"
     end
+
+    begin
+      @client_port, @client_address = Socket.unpack_sockaddr_in(get_peername)
+      puts "New client: #{@client_address}:#{@client_port}"
+    rescue => e
+      p e
+    end
   end # def post_init
 
   #def ssl_handshake_completed
@@ -48,22 +57,29 @@ class Log::Server::Connection < EventMachine::Connection
     #puts get_peer_cert
   #end
 
+  def receive_data(data)
+    @buffer ||= BufferedTokenizer.new
+    @buffer.extract(data).each do |line|
+      receive_line(line.chomp)
+    end
+  end
+
   def _receive_line(line)
     receive_line_raw(line)
   end
 
   def receive_line_raw(line)
-    event = Log::Event.new
-    port, address = Socket.unpack_sockaddr_in(get_peername)
+    #event = Log::Event.new
 
     # RFC3164 section 4.3.3 No PRI or Unidentifiable PRI
-    event.pri = "13"  
+    #event.pri = "13"  
 
     # TODO(sissel): Look for an alternative to Time#strftime since it is
     # insanely slow.
-    event.timestamp = Time.now.strftime("%b %d %H:%M:%S")
-    event.hostname = address
-    event.message = line
+    #event.timestamp = Time.now.strftime("%b %d %H:%M:%S")
+    #event.timestamp = Time.now
+    #event.hostname = @client_address
+    #event.message = line
 
     stats
     #puts event
@@ -72,12 +88,19 @@ class Log::Server::Connection < EventMachine::Connection
   def stats
     @start ||= Time.now
     @count += 1
-    if @count % 10000 == 0
+    if @count % 50000 == 0
       puts "Rate: #{@count / (Time.now - @start)}"
+      @start = Time.now
+      @count = 0
     end
   end
 
   def receive_line_syslog(line)
+    if @client_address == nil
+      @client_port, @client_address = Socket.unpack_sockaddr_in(get_peername)
+      puts "New client: #{@client_address}:#{@client_port}"
+    end
+
     event = Log::Event.new
     if parse_rfc3164(line, event)
     #elsif parse_rfc5424(line, event)
--- server.rb
+++ server.rb
@@ -8,7 +8,7 @@ inputs = [
   Log::Server.new(:tcp, :port => 23424, :protocol => :syslog),
   Log::Server.new(:tcp, :port => 23426, :protocol => :raw),
   Log::Server.new(:udp, :port => 23424),
-  Log::Server.new(:tls, :port => 23425),
+  #Log::Server.new(:tls, :port => 23425),
 ]
 
 EventMachine::run do
